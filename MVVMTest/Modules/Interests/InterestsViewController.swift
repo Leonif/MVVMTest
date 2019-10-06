cfeafad350e5f8f1e58af27040341f97df97c04e
Leonid Nifantyev <leonif1@gmail.com>
2019-10-06 12:02:41 +0300

    colorization + refactoring

--- MVVMTest/Modules/Interests/InterestsViewController.swift
+++ MVVMTest/Modules/Interests/InterestsViewController.swift
@@ -6,30 +6,69 @@
 import UIKit
 
 class InterestsViewController: UIViewController {
-  
-  var viewModel: InterestsViewModelInterface!
-  
+  private var titleLabel: UILabel!
+  private var subtitleLabel: UILabel!
   private var collectionView: UICollectionView!
   private var layout: UICollectionViewFlowLayout!
   var collectionHelper: CollectionHelper?
   
+  var viewModel: InterestsViewModelInterface! {
+    didSet {
+      viewModel.eventHandler = { [weak self] states in
+        switch states {
+        case .fetched:
+          self?.collectionHelper?.reload()
+        case .error:
+          self?.showError()
+        }
+      }
+    }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    self.view.backgroundColor = .yellow
-    setupCollectionView()
+    self.view.backgroundColor = .nero
     
-    viewModel.eventHandler = { [weak self] states in
-      switch states {
-      case .fetched:
-        self?.collectionHelper?.reload()
-      case .error:
-        self?.showError()
-      }
-    }
+    setupTitle()
+    setupSubtitle()
+    setupCollectionView()
     viewModel.fetch()
   }
   
+  
+  
+  
+  private func setupTitle() {
+    self.titleLabel = UILabel(frame: CGRect.zero)
+    self.titleLabel.textAlignment = .center
+    self.titleLabel.text = "Choose interests"
+    self.titleLabel.textColor = .white
+    self.titleLabel.font = UIFont.systemFont(ofSize: 32)
+    self.view.addSubview(titleLabel)
+    titleLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      titleLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 32),
+      titleLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -32),
+      titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 60),
+    ])
+  }
+  
+  private func setupSubtitle() {
+    self.subtitleLabel = UILabel(frame: CGRect.zero)
+    self.subtitleLabel.textAlignment = .center
+    self.subtitleLabel.text = "Choose 5 or more that you like in order to get proper recommendations"
+    self.subtitleLabel.numberOfLines = 0
+    self.subtitleLabel.textColor = .white
+    self.subtitleLabel.font = UIFont.systemFont(ofSize: 20)
+    self.view.addSubview(subtitleLabel)
+    subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      subtitleLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 32),
+      subtitleLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -32),
+      subtitleLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 16),
+    ])
+  }
   func setupCollectionView() {
     self.layout = UICollectionViewFlowLayout()
     self.collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: self.layout)
@@ -41,12 +80,12 @@ class InterestsViewController: UIViewController {
     
     self.view.addSubview(collectionView)
     self.collectionView.translatesAutoresizingMaskIntoConstraints = false
-    self.collectionView.backgroundColor = .red
+    self.collectionView.backgroundColor = .clear
   
     NSLayoutConstraint.activate([
       collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
       collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-      collectionView.topAnchor.constraint(equalTo: self.view.topAnchor),
+      collectionView.topAnchor.constraint(equalTo: self.subtitleLabel.bottomAnchor, constant: 32),
       collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
     ])
   }
--- MVVMTest/Modules/Login/LoginViewController.swift
+++ MVVMTest/Modules/Login/LoginViewController.swift
@@ -11,30 +11,103 @@ import Utils
 
 class LoginViewController: UIViewController {
   
+  private var logoImage: UIImageView!
+  private var titleLabel: UILabel!
+  private var subtitleLabel: UILabel!
   private var loginButton: UIButton!
-  var viewModel: LoginViewModelInterface!
+  
+  var viewModel: LoginViewModelInterface! {
+    didSet {
+      self.viewModel.eventHandler = { [weak self] states in
+        switch states {
+        case .error:
+          self?.showError()
+        case .cancel:
+          self?.show(info: "User cancelled login")
+        }
+      }
+    }
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    self.view.backgroundColor = .red
-    
-    self.viewModel.eventHandler = { [weak self] states in
-      switch states {
-      case .error:
-        self?.showError()
-      case .cancel:
-        self?.show(info: "User canclled login")
-      }
-    }
+    self.setupNavigationBar()
+    self.view.backgroundColor = .nero
     self.setupButton()
+    self.setupSubtitle()
+    self.setupTitle()
+    self.setupIcon()
+    
+  }
+  
+  private func setupNavigationBar() {
+    let nav = self.navigationController?.navigationBar
+    nav?.shadowImage = UIImage()
+    nav?.setBackgroundImage(UIImage(), for: .default)
+    nav?.isTranslucent = true
+    nav?.barTintColor = .clear
+    nav?.backgroundColor = .clear
+  }
+  
+  private func setupIcon() {
+    self.logoImage = UIImageView(image:UIImage(named: "logo"))
+    self.logoImage.contentMode = .scaleAspectFill
+    self.view.addSubview(logoImage)
+    logoImage.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      logoImage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+      logoImage.bottomAnchor.constraint(equalTo: titleLabel.topAnchor, constant: -32),
+      logoImage.heightAnchor.constraint(equalToConstant: 60),
+      logoImage.widthAnchor.constraint(equalToConstant: 60),
+    ])
+  }
+  
+  private func setupTitle() {
+    self.titleLabel = UILabel(frame: CGRect.zero)
+    self.titleLabel.textAlignment = .center
+    self.titleLabel.text = "Welcome to"
+    self.titleLabel.textColor = .white
+    self.titleLabel.font = UIFont.systemFont(ofSize: 32)
+    self.view.addSubview(titleLabel)
+    titleLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      titleLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 32),
+      titleLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -32),
+      titleLabel.bottomAnchor.constraint(equalTo: subtitleLabel.topAnchor, constant: -16),
+    ])
+  }
+  
+  private func setupSubtitle() {
+    self.subtitleLabel = UILabel(frame: CGRect.zero)
+    self.subtitleLabel.textAlignment = .center
+    self.subtitleLabel.text = "SOCIAL MEDIA broadcaster"
+    self.subtitleLabel.textColor = .white
+    self.subtitleLabel.font = UIFont.systemFont(ofSize: 20)
+    self.view.addSubview(subtitleLabel)
+    subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      subtitleLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 32),
+      subtitleLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -32),
+      subtitleLabel.bottomAnchor.constraint(equalTo: loginButton.topAnchor, constant: -70),
+    ])
   }
   
   private func setupButton() {
-    self.loginButton = UIButton(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 100, height: 50)))
-    self.loginButton.setTitle("Login", for: .normal)
+    self.loginButton = UIButton(frame: CGRect.zero)
+    self.loginButton.setTitle("Continue with Facebook", for: .normal)
+    self.loginButton.backgroundColor = .steelBlue
     self.view.addSubview(loginButton)
-    loginButton.center = self.view.center
+  
+    loginButton.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      loginButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 32),
+      loginButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -32),
+      loginButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+      loginButton.heightAnchor.constraint(equalToConstant: 60),
+    ])
+    
+    loginButton.layer.cornerRadius = 30
     
     self.loginButton.addTarget(self, action: #selector(loginTapped), for: .touchUpInside)
   }
@@ -49,7 +122,6 @@ class LoginViewController: UIViewController {
   }
   
   private func show(info: String) {
-    self.alert(message: info)
+    self.alert(title: "App", message: info)
   }
 }
-
